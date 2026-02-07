bade5bc150b8c9500d66d374a4c94259fe7b1dac
Yolanda Piao <ypiao@lcs.on.ca>
2026-02-07 10:00:11 -0500

    Initial Commit

---
+++ BlockLayout/BlockLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlockLayoutApp.swift
+//  BlockLayout
+//
+//  Created by Yolanda Piao on 2/7/26.
+//
+
+import SwiftUI
+
+@main
+struct BlockLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
