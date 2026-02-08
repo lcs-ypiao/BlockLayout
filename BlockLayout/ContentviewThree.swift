754f528440d3de2aa52b5a07d75c3074674081ba
Yolanda Piao <ypiao@lcs.on.ca>
2026-02-08 13:32:28 -0500

    COmpleted practices using blocks (including texts)

---
+++ BlockLayout/ContentviewThree.swift
@@ -0,0 +1,24 @@
+//
+//  ContentviewThree.swift
+//  BlockLayout
+//
+//  Created by Yolanda Piao on 2/7/26.
+//
+
+import SwiftUI
+
+struct ContentviewThree: View {
+    var body: some View {
+        HStack{
+            Rectangle()
+            Rectangle()
+        }
+        VStack{
+            Rectangle()
+        }
+    }
+}
+
+#Preview {
+    ContentviewThree()
+}
---
+++ BlockLayout/Contentviewfour.swift
@@ -0,0 +1,26 @@
+//
+//  Contentviewfour.swift
+//  BlockLayout
+//
+//  Created by Yolanda Piao on 2/7/26.
+//
+
+import SwiftUI
+
+struct Contentviewfour: View {
+    var body: some View {
+        HStack{
+            Rectangle()
+            Rectangle()
+            VStack {
+                Rectangle()
+                Rectangle()
+                Rectangle()
+            }
+        }
+    }
+}
+
+#Preview {
+    Contentviewfour()
+}
